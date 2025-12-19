9b8b916a843534d9bc327c9f6c7ba2a037b962e2
SUDEESH K <sudeeshkumarkm21@gmail.com>
2025-12-19 13:04:20 +0530

    Add ForgotPassword route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import './App.css'; // Ensure CSS is imported
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
   return (
@@ -27,6 +28,9 @@ function App() {
             <Route element={<ProtectedRoute />}>
               <Route path="/dashboard" element={<Dashboard />} />
             </Route>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
             
             {/* Default redirect to login if path is empty */}
             <Route path="/" element={<Login />} />
